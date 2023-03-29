1c915cf3d099acc48b650825c42dbdfb95b436a2
EngCycerow <cycerow@gmail.com>
2023-03-29 15:33:03 -0300

    Update Regressão_Logística_Binária_e_Multinomial.r

--- Modelos Supervisionados/Regressão_Logistica_Binária_e_Multinomial/Regressão_Logística_Binária_e_Multinomial.r
+++ Modelos Supervisionados/Regressão_Logistica_Binária_e_Multinomial/Regressão_Logística_Binária_e_Multinomial.r
@@ -1,2 +1,45 @@
 # Modelo Logístico Binário e Multinomial
-print('eu')
+##################################################################################
+#                  INSTALAÇÃO E CARREGAMENTO DE PACOTES NECESSÁRIOS             #
+##################################################################################
+#Pacotes utilizados
+pacotes <- c("plotly","tidyverse","knitr","kableExtra","fastDummies","rgl","car",
+             "reshape2","jtools","stargazer","lmtest","caret","pROC","ROCR","nnet",
+             "magick","cowplot","globals","equatiomatic")
+
+options(rgl.debug = TRUE)
+
+if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
+  instalador <- pacotes[!pacotes %in% installed.packages()]
+  for(i in 1:length(instalador)) {
+    install.packages(instalador, dependencies = T)
+    break()}
+  sapply(pacotes, require, character = T) 
+} else {
+  sapply(pacotes, require, character = T) 
+}
+
+
+##############################################################################
+#                   REGRESSÃO LOGÍSTICA BINÁRIA - PARTE CONCEITUAL           #
+##############################################################################
+#Estabelecendo uma função para a probabilidade de ocorrência de um evento
+prob <- function(z){
+  prob = 1 / (1 + exp(-z))
+}
+
+#Plotando a curva sigmóide teórica de ocorrência de um evento para um range
+#do logito z entre -5 e +5
+data.frame(z = -5:5) %>%
+  ggplot() +
+  stat_function(aes(x = z, color = "Prob. Evento"),
+                fun = prob,
+                size = 2) +
+  geom_hline(yintercept = 0.5, linetype = "dotted") +
+  scale_color_manual("Legenda:",
+                     values = "#440154FF") +
+  labs(x = "Logito z",
+       y = "Probabilidade") +
+  theme_bw()
+
+
